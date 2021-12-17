3872a58305c1a871db483064b1c3ac971e37d7f7
Дмитрий Хижняков <ya.dihov@yandex.ru>
2021-12-17 22:49:42 +0300

    а так?

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,21 +1,32 @@
 import django_filters
+from django.contrib.auth import get_user_model
 
-from .models import Favorite, Ingredient, Recipe, ShoppingList
+from .models import Ingredient, Recipe
+
+User = get_user_model()
 
 
 class RecipeFilter(django_filters.FilterSet):
-    author = django_filters.NumberFilter()
+    author = django_filters.NumberFilter(queryset=User.objects.all())
     tags = django_filters.AllValuesMultipleFilter(field_name='tags__slug')
-    is_favorited = django_filters.ModelChoiceFilter(
-        queryset=Favorite.objects.all()
-    )
-    is_in_shopping_cart = django_filters.ModelChoiceFilter(
-        queryset=ShoppingList.objects.all()
+    is_favorited = django_filters.CharFilter(method='get_is_favorited')
+    is_in_shopping_cart = django_filters.CharFilter(
+        method='get_is_in_shopping_cart'
     )
 
     class Meta:
         model = Recipe
         fields = ('author', 'tags')
+
+    def get_is_favorited(self, queryset, name, value):
+        if value is True:
+            return queryset.filter(favorites_recipes=self.request.user)
+        return None
+
+    def get_is_in_shopping_cart(self, queryset, name, value):
+        if value is True:
+            return queryset.filter(shopping_user=self.request.user)
+        return None
 
 
 class IngredientFilter(django_filters.FilterSet):
