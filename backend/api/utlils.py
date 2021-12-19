ae21cd6890f9c80e19c810f2f8975050c1d8de1f
Дмитрий Хижняков <ya.dihov@yandex.ru>
2021-12-19 23:33:02 +0300

    utils

--- backend/api/utlils.py
+++ backend/api/utlils.py
@@ -1,17 +1,16 @@
-from .models import Ingredient, IngredientAmount
+from .models import IngredientAmount
 
 
 def ingredient_creaton(recipe, ingredients):
     amounts_instance = []
     for ingredient_data in ingredients:
         amount = ingredient_data['amount']
+        ingredient = ingredient_data['id']
         amounts_instance.append(
             IngredientAmount(
                 amount=amount,
                 recipe=recipe,
-                ingredient=Ingredient.objects.get(
-                    pk=ingredient_data['id']
-                ),
+                ingredient=ingredient
             )
         )
     return IngredientAmount.objects.bulk_create(amounts_instance)
